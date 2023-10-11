396029ad7fc7f0b4e3990136cda7fa9aeca37e89
fimaAidarous <fma0899@gmail.com>
2023-10-11 11:08:36 -0700

    first commit

---
+++ AssetMSProject/Controllers/AppRoleController1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AssetMSProject.Controllers
+{
+    [Authorize]
+    public class AppRoleController : Controller
+    {
+        
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AppRoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        //List All the roles created by users
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+
+        [HttpPost]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(IdentityRole model) 
+        {
+            // avoid duplicate role
+            if(!_roleManager.RoleExistsAsync(model.Name).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(model.Name)).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");  
+        }
+    }
+}
---
+++ AssetMSProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AssetMSProject.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AssetMSProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> applicationUsers { get; set; }    
+    }
+}
